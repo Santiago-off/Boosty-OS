69eaaa6f7b625700df05021897063a78858dae3b
Santiago <santiagorfernandezcv@gmail.com>
2026-01-20 17:43:14 +0100

    1.0

--- WEB/js/firebase-config.js
+++ WEB/js/firebase-config.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDObeU5b4_JpF4kCVC_gh8Nrv-nnyVhRjg",
+  authDomain: "boosty-os-4b54d.firebaseapp.com",
+  projectId: "boosty-os-4b54d",
+  storageBucket: "boosty-os-4b54d.firebasestorage.app",
+  messagingSenderId: "877073195920",
+  appId: "1:877073195920:web:234a56b3dfed233d844915"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export { app };
+export { app, db };
